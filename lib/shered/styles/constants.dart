930e9e97f12c7d6a01246cb76731109698500871
mohanedSalaheldin <mohnsd1162002@gmail.com>
2023-10-08 16:44:58 +0300

    ALL DONE ALHAMDULILAH

--- lib/shered/styles/constants.dart
+++ lib/shered/styles/constants.dart
@@ -2,3 +2,5 @@ import 'package:hexcolor/hexcolor.dart';
 
 var lightScaffoldColor = HexColor('#f7f8fb');
 var defaultColor = HexColor('#25bfff');
+
+var darkScaffoldColor = HexColor('#17181a');
--- lib/shered/styles/themes.dart
+++ lib/shered/styles/themes.dart
@@ -1,17 +1,66 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:calculator_ui/shered/styles/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 var lightTheme = ThemeData(
   appBarTheme: AppBarTheme(
-    color:lightScaffoldColor,
+    color: lightScaffoldColor,
     elevation: 0.0,
   ),
   fontFamily: 'Poppins',
   scaffoldBackgroundColor: lightScaffoldColor,
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 40,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 20,
+      overflow: TextOverflow.ellipsis,
+      color: Colors.black,
+    ),
+  ),
+  filledButtonTheme: FilledButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.white),
+      foregroundColor: MaterialStateProperty.all(defaultColor),
+      textStyle: MaterialStateProperty.all(
+        TextStyle(
+          color: defaultColor,
+          fontSize: 24,
+        ),
+      ),
+    ),
+  ),
+);
+var darkTheme = ThemeData(
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.amber,
+  ),
+  appBarTheme: AppBarTheme(
+    color: darkScaffoldColor,
+    elevation: 0.0,
+  ),
+  fontFamily: 'Poppins',
+  scaffoldBackgroundColor: darkScaffoldColor,
   textTheme: TextTheme(
     bodyMedium: TextStyle(
-        // fontSize:
-        ),
+      fontSize: 40,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 20,
+      overflow: TextOverflow.ellipsis,
+      color: Colors.white,
+    ),
+  ),
+  filledButtonTheme: FilledButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.white),
+    ),
   ),
 );
